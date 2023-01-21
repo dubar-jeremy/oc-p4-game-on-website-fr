4cfa59c1054ec0e701a32f44560582d370cef6ed
Dubar Jérémy <jeremy.dubar@decathlon.com>
2023-01-21 11:17:55 +0100

    fix html and add jsdoc

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,17 +12,44 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const closeBtn = document.querySelector('.close');
 const formData = document.querySelectorAll(".formData");
+const form = document.querySelector("form[name='reserve']");
 
-// launch modal event
+
+// listeners
+
+/**
+ @listener
+ @param {HTMLElement} modalBtn - An element with the class ".modal-btn"
+ @event click - when the element is clicked, the "launchModal" function is called
+ */
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
-// launch modal form
+
+
+/**
+@listener
+@param {HTMLElement} closeBtn - An element with the class ".close"
+@event click - when the element is clicked, the "closeModal" function is called
+*/
+closeBtn.addEventListener('click', closeModal)
+
+
+
+// functions 
+
+/** 
+ @function launchModal
+ @description This function makes the modal visible by setting the display property of an element with the id "modalbg" to "block".
+ */
 function launchModal() {
   modalbg.style.display = "block";
 }
 
-// close modal form
-closeBtn.addEventListener('click', () => {
-  modalbg.style.display = "none";
-});
 
+/**
+@function closeModal
+@description This function hides the modal by setting the display property of an element with the id "modalbg" to "none".
+*/
+function closeModal() {
+  modalbg.style.display = "none";
+}
